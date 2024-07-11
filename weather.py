59d95bd9203dee9070c27484189742a65b6a6939
Michael Chiaramonte <m6chiaramonte@gmail.com>
2024-07-11 19:19:31 -0400

    Fixed a bug where forecastResult wasn't being used and also a bug where the content-type was including chartset unexepectedly for one request but not the other.

--- weather.py
+++ weather.py
@@ -126,14 +126,14 @@ while var == 1 :
     time.sleep(1800)
     while True :
         result = requests.get("https://lightning.ambientweather.net/devices?public.slug=19f3efb7371679fea5c94c6733e52d7b")
-        if not result.headers.get("content-type") == "application/json" :
+        if not result.headers.get("content-type") == "application/json; charset=utf-8" :
             time.sleep(5)
             continue
     
         result = result.json()["data"][0]["lastData"];
 
         forecastResult = requests.get("https://www.weatherlink.com/embeddablePage/getData/acf9850534924ff0915ce847633ab609")
-        if not result.headers.get("content-type") == "application/json" : 
+        if not forecastResult.headers.get("content-type") == "application/json" : 
             time.sleep(5)
             continue
         forecastResult = forecastResult.json()
